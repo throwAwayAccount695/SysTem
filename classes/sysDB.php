3d6345f571a511e2cbb8c2107304ce439e505c06
throwAwayAccount695 <jako6981@tietgen.dk>
2020-04-14 12:49:48 +0200

    Feat: added get_row class method

--- classes/sysDB.php
+++ classes/sysDB.php
@@ -7,5 +7,21 @@ use Mysqli;
             $conn = new mysqli($db_host, $db_user, $db_pwd, $db_name);
             $this->conn = $conn;
         }
+
+        public function get_row($table_name, $where, $data = 'OBJECT'){
+            $sql = "SELECT * FROM $table_name WHERE $where";
+            $result = $this->conn->query($sql);
+            switch ($data) {
+                case 'OBJECT':
+                    return $result->fetch_object();
+                    break;
+                case 'ARRAY_A':
+                    return $result->fetch_assoc();
+                    break;
+                case 'ARRAY_N':
+                    return $result->fetch_array();
+                    break;
+            }
+        }
     }
 ?>
--- header.php
+++ header.php
@@ -3,4 +3,6 @@
     require_once("classes/sysDB.php");
 
     $db = new \SysTem\SysDB(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    print_r($db->get_row('cars', 'id = 2'));
 ?>
