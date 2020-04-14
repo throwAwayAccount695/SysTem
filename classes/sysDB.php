5205fb9566c518a75fbc2076887f7428151f4a36
throwAwayAccount695 <jako6981@tietgen.dk>
2020-04-14 12:29:03 +0200

    Fix: fixed namespace error

--- classes/sysDB.php
+++ classes/sysDB.php
@@ -1,9 +1,11 @@
 <?php namespace SysTem;
+use Mysqli;
 
     class SysDB{
+        public $conn;
         public function  __construct($db_host, $db_user, $db_pwd, $db_name){
             $conn = new mysqli($db_host, $db_user, $db_pwd, $db_name);
-            return  $conn;
+            $this->conn = $conn;
         }
     }
 ?>
--- header.php
+++ header.php
@@ -2,5 +2,5 @@
     require_once("sys-config.php"); 
     require_once("classes/sysDB.php");
 
-    $db = new SysTem\SysDB(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $db = new \SysTem\SysDB(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 ?>
