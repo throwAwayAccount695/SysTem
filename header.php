2fcaf86891d8d8657fa9069f44fce654b75b87b7
throwAwayAccount695 <jako6981@tietgen.dk>
2020-04-14 13:08:01 +0200

    Fix: file name

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
     require_once("sys-config.php"); 
-    require_once("classes/sysDB.php");
+    require_once("classes/SysDB.php");
 
     $db = new \SysTem\SysDB(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
