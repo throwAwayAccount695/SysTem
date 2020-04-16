d5dbb113fdab80e674bd088d274f28ba4a4cafad
throwAwayAccount695 <jako6981@tietgen.dk>
2020-04-16 11:35:14 +0200

    [Feat]: added get_results function with docblock [Fix]: fetch_array getting both assoc and numbered insted of just numbered.

--- classes/sysDB.php
+++ classes/sysDB.php
@@ -27,6 +27,8 @@ use Mysqli;
          * @param string $table_name table_name is the name of the table you want to search in.
          * @param string $where where you want the row from, usually by id (typing WHERE is not needed).
          * @param string $data data is what you want the data to be contained in. your choices are; OBJECT (default), ARRAY_N (numbered array) or ARRAY_A (assoc array). 
+         * 
+         * @return array $result is return as one of three choosen array types.
          */
         public function get_row($table_name, $where, $data = 'OBJECT'){
             $sql = "SELECT * FROM $table_name WHERE $where";
@@ -40,7 +42,7 @@ use Mysqli;
                         return $result->fetch_assoc();
                         break;
                     case 'ARRAY_N':
-                        return $result->fetch_array();
+                        return $result->fetch_array(MYSQLI_NUM);
                         break;
                 }
             } else {
@@ -53,17 +55,40 @@ use Mysqli;
          * 
          * @param string $col_name col_name is the name of the wanted column or columns (split column names with a comma).
          * @param string $table_name table_name is the table you want to search in.
+         * 
+         * @return array $data is return as an numbered array.
          */
         public function get_col($col_name, $table_name){
             $sql = "SELECT $col_name FROM $table_name";
             $result = $this->conn->query($sql);
             if(!empty($result)){
                 $data = array();
-                while($row = mysqli_fetch_array($result)) {
+                while($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
                     $data[] = $row;
                 }
                 return $data;
             } else {
+                return FALSE;
+            }
+        }
+
+        /**
+         * Make a custom SQL string
+         * 
+         * @param string $sql_string The function has no SQL premade, all SQL have to be given in this parameter.
+         * 
+         * @return array $data is return as an numbered array of objects.
+         */
+        public function get_results($sql_string){
+            $result = $this->conn->query($sql_string);
+            if(!empty($result)){
+                $data = array();
+                while($row = mysqli_fetch_object($result)){
+                    $data[] = $row;
+                }
+                return $data;
+            } else {
+                throw new Exception("");
                 return FALSE;
             }
         }
--- header.php
+++ header.php
@@ -3,8 +3,10 @@
     require_once("classes/SysDB.php");
 
     $db = new \SysTem\SysDB(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-    print_r($db->get_row('cars', 'id = 20'));
-    echo '<br>';
-    print_r($db->get_col('id', 'cars'));
+    echo '<br><pre>';
+    print_r($db->get_row('cars', 'id = 5', 'OBJECT'));
+    echo '</pre>';
+    echo '<br><pre>';
+    print_r($db->get_results("SELECT * FROM CARS WHERE id < 10"));
+    echo '</pre>';
 ?>
